64dcfa9c982169e96483ff432b7403ae2d0b6acd
caiopy <caiop.sci@hotmail.com>
2022-09-05 18:36:42 -0300

    Notification Button Component

---
+++ frontend/src/components/NotificationButton/index.tsx
@@ -0,0 +1,11 @@
+import icon from '../../assets/images/notification-icon.svg'
+import 'styles.css'
+
+export const NotificationButton = () => {
+  return(
+    <div className="dsmeta-red-btn">
+      <img src={icon} alt="Notificar" />
+    </div>
+  )
+}
+
