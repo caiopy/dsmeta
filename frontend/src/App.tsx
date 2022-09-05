2e2ed103e4e1c054bc7fdb2548fc9c002dd109bf
caiopy <caiop.sci@hotmail.com>
2022-09-05 19:58:04 -0300

    Header and Static Sales Card Table Components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,19 @@
+import { Header } from "./components/Header";
+import { SalesCard } from "./components/SalesCard";
 
 function App() {
-
   return (
-    <div>
-
-    </div>
-  )
+    <>
+      <Header />
+      <main>
+        <section id="sales">
+          <div className="dsmeta-container">
+            <SalesCard />
+          </div>
+        </section>
+      </main>
+    </>
+  );
 }
 
-export default App
+export default App;
--- frontend/src/components/NotificationButton/index.tsx
+++ frontend/src/components/NotificationButton/index.tsx
@@ -1,11 +1,10 @@
-import icon from '../../assets/images/notification-icon.svg'
-import 'styles.css'
+import icon from "../../assets/images/notification-icon.svg";
+import "./styles.css";
 
 export const NotificationButton = () => {
-  return(
+  return (
     <div className="dsmeta-red-btn">
       <img src={icon} alt="Notificar" />
     </div>
-  )
-}
-
+  );
+};
