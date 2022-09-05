42f071b25986b212c222842bd2398f90b90b3870
caiopy <caiop.sci@hotmail.com>
2022-09-05 17:49:38 -0300

    project clean

---
+++ backend/src/test/java/com/caiop/dsmeta/DsmetaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.caiop.dsmeta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsmetaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
